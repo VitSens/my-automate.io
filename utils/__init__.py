616098a170a7d0ee8e8f63c766adbc251b1321ac
VitSens <55030208+VitSens@users.noreply.github.com>
2022-08-15 23:45:54 +0300

    add utility class for parse date to GoogleCalendar format

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.date_parser import Parser
---
+++ utils/date_parser.py
@@ -0,0 +1,18 @@
+class Parser(object):
+
+    @staticmethod
+    def lex(line):
+        lines = line.strip().split(' ')
+
+        default = 'RRULE:'
+
+        recurrenceType = {
+            'every': 'FREQ=',
+            'day': 'DAILY;',
+        }
+
+        for lan in lines:
+            if recurrenceType.get(lan):
+                default += recurrenceType.get(lan)
+
+        return default
